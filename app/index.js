d2a6436e6466b35f4af59f64629edebbe47d2e51
Anton Sidelnikov <ndmeredian@gmail.com>
2018-04-10 17:59:54 +0600

    Implement p2p-server

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../lib/blockchain');
+const P2pServer = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
 
@@ -17,9 +19,13 @@ app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log("New block added: ", block);
 
+    p2pServer.syncChains();
+
     res.redirect('/blocks');
 })
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`);
 });
+
+p2pServer.listen();
